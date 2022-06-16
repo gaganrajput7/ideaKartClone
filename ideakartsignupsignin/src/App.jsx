7705381b0b41d17226be9db494e7bf275d7b063e
Alim <alimabbas44891@gmail.com>
2022-06-16 08:57:46 +0530

    sign-in sign-up

---
+++ ideakartsignupsignin/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Signin from './components/SignIn/Signin';
+import Signup from './components/SignUp/Signup';
+
+
+function App() {
+  return (
+    <div className="App">
+        {/* <Signin></Signin> */}
+        <Signup></Signup>
+    </div>
+  );
+}
+
+export default App;
---
+++ ideakartsignupsignin/src/components/SignUp/Signup.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import "./signUp.css";
+import axios from "axios";
+// import { useNavigate  } from "react-router-dom";
+
+const Signup = () => {
+
+    // const navigate = useNavigate ();
+
+    const [ user, setUser] = useState({
+        name: "",
+        email:"",
+        mobile:"",
+        password:"",
+        reEnterPassword: ""
+    });
+
+    const handleChange = e => {
+        const { name, value } = e.target;
+        setUser({
+            ...user,
+            [name]: value
+        });
+    };
+
+    const Signup = () => {
+        const { name, email,mobile, password, reEnterPassword } = user
+        if( name && email && mobile && password && (password === reEnterPassword)){
+            axios.post("http://localhost:8080/Signup", user)
+            .then( res => {
+                alert(res.data.message);
+                // navigate.push("/signIn");
+            });
+        } else {
+            alert("invlid input");
+        }
+        
+    };
+
+    return (
+        <div className="signUp">
+            {console.log("User", user)}
+            <h1>signUp</h1>
+            <input type="text" name="name" value={user.name} placeholder="Your Name" onChange={ handleChange }></input>
+            <input type="text" name="email" value={user.email} placeholder="Your Email" onChange={ handleChange }></input>
+            <input type="text" name="mobile" value={user.mobile} placeholder="Your mobile Number" onChange={ handleChange }></input>
+            <input type="password" name="password" value={user.password} placeholder="Your Password" onChange={ handleChange }></input>
+            <input type="password" name="reEnterPassword" value={user.reEnterPassword} placeholder="Re-enter Password" onChange={ handleChange }></input>
+            <div className="button" onClick={Signup}>signUp</div>
+            <div>or</div>
+            {/* <div className="button" onClick={() =>navigate.push("/signIn")}>Login</div> */}
+        </div>
+    )
+}
+
+export default Signup;
