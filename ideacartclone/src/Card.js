9be08767364bd6135655d6197c95e7b83ce34789
amansingh1712 <aman.iit96@gmail.com>
2022-06-18 14:11:26 +0530

    Backend Connected

--- ideacartclone/src/Card.js
+++ ideacartclone/src/Card.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import styles from './card.module.css'
+import styles from './card.module.css';
+import link from 'react-router-dom';
 
 function Card(props) {
     const row = props.row;
     return (
-        <div className={styles.card}>
+        <div className={styles.card} >
 
 
             <div className={styles.cardImg}>
@@ -14,26 +15,36 @@ function Card(props) {
 
             </div>
 
-            <div className={styles.cardDescription}>
+            <div className={styles.cardDetails}>
 
-                {row.discription.substring(0, 50)}
+
+                <div className={styles.Name}>
+
+                    {row.name}
+
+                </div>
+
+                <div className={styles.cardPrice}>
+                    {row.price}
+                </div>
+                <div className={styles.cardHorizontalLine}></div>
+
+                <div className={styles.cardIcons}>
+
+                    <div className={styles.cardCartIcon}><img src="/shopping-cart.png" alt="cartIcon" />< > View Now</></div>
+                    <div className={styles.cardVerticalLine}></div>
+                    <div className={styles.cardBulletIcon}><img src="/bullet-list.png" alt="bulletIcon" /> <a a href="#" > More Details</a></div>
+                </div>
 
             </div>
-
-            <div className={styles.cardPrice}>
-                {row.price}
-            </div>
-            <div className={styles.cardIcons}>
-
-                <div className={styles.cardCartIcon}><img src="/shopping-cart.png" alt="cartIcon" /></div>
-                <div className={styles.cardBulletIcon}><img src="/bullet-list.png" alt="bulletIcon" /></div>
-            </div>
-
 
 
 
         </div>
     )
 }
-
 export default Card
+
+
+
+
