efa5ba9811e421982d8d67107cc643bef49dc3e8
NikeshBorkar <99666800+NikeshBorkar@users.noreply.github.com>
2022-06-18 15:09:30 +0530

    cartpage

---
+++ addtocartreact-main/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import Cart from "./component/Cart";
+import Cart from "./components/Cart";
+
+const App = () => {
+  return (
+    <>
+      <Cart />
+    </>
+  );
+};
+
+export default App;
